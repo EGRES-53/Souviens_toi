baf4908e02139e136b539bc6242836b36027e450
EGRES-53 <dani.sdo53@outlook.com>
2025-09-17 15:17:22 +0200

    Update App.tsx
    "complété version"

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
 import HomePage from './pages/HomePage';
 import TimelinePage from './pages/TimelinePage';
 import LoginPage from './pages/LoginPage';
@@ -11,19 +12,20 @@ import NotFoundPage from './pages/NotFoundPage';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/timeline" element={<TimelinePage />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/register" element={<RegisterPage />} />
-      <Route path="/gallery" element={<GalleryPage />} />
-      <Route path="/stories" element={<StoriesPage />} />
-      <Route path="/profile" element={<ProfilePage />} />
-      <Route path="*" element={<NotFoundPage />} />
-    </Routes>
+    <div className="min-h-screen bg-gray-50">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/timeline" element={<TimelinePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/gallery" element={<GalleryPage />} />
+        <Route path="/stories" element={<StoriesPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </div>
   );
 }
-
-export default App;
 
 
